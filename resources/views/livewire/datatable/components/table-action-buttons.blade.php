456fa1f6eb06700d252bd73eb4707303336a7e53
JV <preubecitas@gmail.com>
2023-11-09 18:01:01 -0600

    [new] Botones de importación (sin función) y sección para los filtros (sin función)

---
+++ resources/views/livewire/datatable/components/table-action-buttons.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <button
+        class="flex items-center h-10 bg-transparent p-2 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded"
+        @click="expanded = !expanded">
+        <x-fas-file-excel class="w-5 h-5" />
+        {{ __('export') }}
+    </button>
+</div>
+<div>
+    <button
+        class="flex items-center h-10 bg-transparent p-2 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded"
+        @click="expanded = !expanded">
+        <x-fas-file-pdf class="w-5 h-5" />
+        {{ __('print') }}
+    </button>
+</div>
+<div>
+    <button
+        class="bg-transparent h-10 p-2 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded"
+        @click="expanded = !expanded">
+        <x-zondicon-filter class="w-5 h-5" />
+    </button>
+</div>
--- resources/views/livewire/datatable/main.blade.php
+++ resources/views/livewire/datatable/main.blade.php
@@ -2,26 +2,10 @@
     <section class="w-full">
         <div class="max-w-screen">
             <!-- Start coding here -->
-            <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-                <div x-data="{ expanded: false }">
-                    <button
-                        class="bg-transparent p-2 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded"
-                        @click="expanded = ! expanded">
-                        <x-zondicon-filter class="w-5 h-5" />
-                    </button>
-
-                    <div x-data="{ expanded: false }" x-show="expanded"
-                        x-transition:enter="transform ease-out duration-300 transition"
-                        x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-                        x-transition:enter-end="translate-y-0 opacity-100 sm:translate-x-0"
-                        x-transition:leave="transition ease-in duration-100" x-transition:leave-start="opacity-100"
-                        x-transition:leave-end="opacity-0"
-                        class="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto">
-                        lorem ispm
-                    </div>
-                </div>
+            <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden"
+                x-data="{ expanded: false }">
                 <div class="flex items-center justify-between d p-4">
-                    <div class="flex">
+                    <div class="flex w-full px-4">
                         <div class="relative w-full">
                             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                 <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-100"
@@ -32,27 +16,27 @@
                                 </svg>
                             </div>
                             <input wire:model.live.debounce.300ms="search" type="text"
-                                class="text-gray-800 dark:text-gray-200 border bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-600 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 "
+                                class="h-10 text-gray-800 dark:text-gray-200 border bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-600 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2"
                                 placeholder="Search" required="">
                         </div>
-                        <div class="relative px-4">
-
-                        </div>
                     </div>
 
+                    <!-- Action Buttons (include filter button) -->
                     <div class="flex space-x-3">
-                        <div class="flex space-x-3 items-center">
-                            <label
-                                class="w-40 text-sm font-medium text-gray-800 dark:text-gray-200">{{ __('User Type :') }}</label>
-                            <select
-                                class="text-gray-800 dark:text-gray-200 border bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                                <option value="">All</option>
-                                <option value="0">User</option>
-                                <option value="1">Admin</option>
-                            </select>
-                        </div>
+                        @include('livewire.datatable.components.table-action-buttons')
                     </div>
                 </div>
+
+                <!-- Filters Section -->
+                <div x-data="{ ghost: false }" x-show="expanded"
+                    x-transition:enter="transform ease-in duration-300 transition"
+                    x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+                    x-transition:enter-end="translate-y-0 opacity-100 sm:translate-x-0"
+                    x-transition:leave="transition ease-out duration-100" x-transition:leave-start="opacity-100"
+                    x-transition:leave-end="opacity-0" class="w-full pointer-events-auto p-4">
+                    @include('livewire.datatable.components.table-filters')
+                </div>
+
                 <div class="overflow-x-auto">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-100">
                         <thead
@@ -113,6 +97,7 @@
                                 class="w-32 text-sm font-medium text-gray-800 dark:text-gray-200">{{ __('Show') }}</label>
                             <select wire:model.live='perPage'
                                 class="text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                                <option value="7">7</option>
                                 <option value="10">10</option>
                                 <option value="20">20</option>
                                 <option value="50">50</option>
