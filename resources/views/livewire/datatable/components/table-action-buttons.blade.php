1b69305d924622d83ac3b23ee0d142ebebd85831
JV <preubecitas@gmail.com>
2023-11-10 18:20:55 -0600

    [add] cambios de color a los botones de acción de la tabla

--- resources/views/livewire/datatable/components/table-action-buttons.blade.php
+++ resources/views/livewire/datatable/components/table-action-buttons.blade.php
@@ -1,6 +1,6 @@
 <div>
     <button
-        class="flex items-center h-10 bg-transparent p-2 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded"
+        class="flex items-center h-10 bg-transparent p-2 hover:bg-green-500 text-green-700 font-semibold hover:text-white border border-green-500 hover:border-transparent rounded"
         @click="expanded = !expanded">
         <x-fas-file-excel class="w-5 h-5" />
         {{ __('export') }}
@@ -8,7 +8,7 @@
 </div>
 <div>
     <button
-        class="flex items-center h-10 bg-transparent p-2 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded"
+        class="flex items-center h-10 bg-transparent p-2 hover:bg-yellow-500 text-yellow-700 font-semibold hover:text-white border border-yellow-500 hover:border-transparent rounded"
         @click="expanded = !expanded">
         <x-fas-file-pdf class="w-5 h-5" />
         {{ __('print') }}
