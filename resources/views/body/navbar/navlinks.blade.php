a644675a7165b5123aa83f2203d370a8fb4cd07d
K <JuanEscobarGT@outlook.com>
2023-11-11 17:24:40 -0600

    [backup] Cambios para compatibilidad del código hecho fuera de línea

--- resources/views/body/navbar/navlinks.blade.php
+++ resources/views/body/navbar/navlinks.blade.php
@@ -1,11 +1,16 @@
+@php
+    // The pages to active the navbar option
+    $adminRoutes = ['list.user'];
+@endphp
+
 <!-- Direct option -->
-<x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+<x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
     {{ __('Dashboard') }}
 </x-nav-link>
 
 <!-- Admin Menú -->
 @if (auth()->user()->can('view_menu_admin') || auth()->user()->admin)
-    <x-dropdown-nav align="left" width="48" :active="request()->routeIs('profidle.edit', 'sapo', 'profile.edit')">
+    <x-dropdown-nav align="left" width="48" :active="request()->routeIs($adminRoutes)">
         @include('body.navbar.menu.admin')
     </x-dropdown-nav>
 @endif
