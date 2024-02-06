7871e3de5a48bc060e1a76959b690f267468f160
JV <preubecitas@gmail.com>
2024-02-06 13:20:11 -0600

    [add] Activa el menú en el navbar administrativo para roles y permisos

--- resources/views/backend/sidebars/list_admin_user.blade.php
+++ resources/views/backend/sidebars/list_admin_user.blade.php
@@ -2,19 +2,19 @@
     <div class="h-full overflow-y-auto ">
         <ul class="space-y-2 font-medium">
             <li>
-                <x-sidenav-link :active="request()->routeIs('list.user')" :href="route('list.user')" wire:navigate>
+                <x-sidenav-link :active="request()->routeIs('list.user')" :href="route('list.user')">
                     <x-fas-users class="w-5 h-5" />
                     <span class="ms-3">{{ __('users') }}</span>
                 </x-sidenav-link>
             </li>
             <li>
-                <x-sidenav-link :active="request()->routeIs('list.role')" :href="route('list.role')" wire:navigate>
+                <x-sidenav-link :active="request()->routeIs('list.role')" :href="route('list.role')">
                     <x-fas-users class="w-5 h-5" />
                     <span class="ms-3">{{ __('roles') }}</span>
                 </x-sidenav-link>
             </li>
             <li>
-                <x-sidenav-link :active="request()->routeIs('list.permission')" :href="route('list.permission')" wire:navigate>
+                <x-sidenav-link :active="request()->routeIs('list.permission')" :href="route('list.permission')">
                     <x-fas-users class="w-5 h-5" />
                     <span class="ms-3">{{ __('permissions') }}</span>
                 </x-sidenav-link>
